91085bd33d42906920f1a09cf307d5d239c5293b
Fizzpig <xlfizzpiglx@gmail.com>
2024-06-03 15:08:26 +0930

    test

--- test.py
+++ test.py
@@ -8,9 +8,11 @@ SCREEN_HEIGHT = 600
 font = pygame.font.SysFont('Minecraft', 40)
 cost = font.render("2", 0, (0, 0, 0))
 name = font.render("Lightning", 0, (0, 0, 0))
-description = font.render("Deal 2 Damage, Temporarily stuns the enemy, disabling it from using attacking moves for "
-                          "the turn.", 0, (0, 0, 0))
-#text.fill((255,0,0))
+font = pygame.font.SysFont('Minecraft', 30)
+description = font.render.autofit.text("Deal 2 Damage, Temporarily stuns the enemy, disabling it from using attacking "
+                                       "moves for the turn.", 0, (0, 0, 0))
+# text.fill((255,0,0))
+#test
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Test")
@@ -27,7 +29,7 @@ while run:
     screen.blit(surface, (0, 0))
     screen.blit(cost, (65, 58))
     screen.blit(name, (115, 59))
-    screen.blit(description, (115, 59))
+    screen.blit(description, (42, 300))
     pygame.display.update()
 pygame.quit()
 sys.exit()
