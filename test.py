72b7f8535bb64db6668e2f71ff6893c7c062d1ee
Fizzpig <xlfizzpiglx@gmail.com>
2024-06-03 14:51:44 +0930

    Added Cards

--- test.py
+++ test.py
@@ -1,29 +1,33 @@
-# class A:
-#     mod = []
-#
-#     def __init__(self):
-#         self.mod.append("yes")
-#
-#
-# class C:
-#     mod = []
-#
-#     def __init__(self):
-#         self.mod.append('maybe')
-#
-#
-# class B(A, C):
-#     # mod = ['no']
-#
-#     def __init__(self):
-#         A.__init__(self)
-#         C.__init__(self)
-#
-#
-# a = B()
-#
-# print(a.mod)
+import pygame
+import sys
 
-a={}
-a['s']=1
-print(a)
+pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+font = pygame.font.SysFont('Minecraft', 40)
+cost = font.render("2", 0, (0, 0, 0))
+name = font.render("Lightning", 0, (0, 0, 0))
+description = font.render("Deal 2 Damage, Temporarily stuns the enemy, disabling it from using attacking moves for "
+                          "the turn.", 0, (0, 0, 0))
+#text.fill((255,0,0))
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Test")
+
+surface = pygame.image.load("data/assets/cards/Common Lightning.png")
+surface_tranformed = pygame.transform.scale(surface, (184, 165))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    screen.fill((255, 255, 255))
+    screen.blit(surface, (0, 0))
+    screen.blit(cost, (65, 58))
+    screen.blit(name, (115, 59))
+    screen.blit(description, (115, 59))
+    pygame.display.update()
+pygame.quit()
+sys.exit()
