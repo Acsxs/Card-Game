a5c5aa41e92f955f98813efc65af3783a0817374
Fizzpig <xlfizzpiglx@gmail.com>
2024-06-05 12:16:20 +0930

    added playing board assets

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-import textwrap
 
 pygame.init()
 
@@ -9,7 +8,7 @@ SCREEN_HEIGHT = 600
 font = pygame.font.SysFont('Minecraft', 40)
 cost = font.render("2", 0, (0, 0, 0))
 name = font.render("Lightning", 0, (0, 0, 0))
-font = pygame.font.SysFont('Minecraft', 30)
+# font = pygame.font.SysFont('Minecraft', 26)
 description = "Deal 2 Damage, Temporarily stuns the enemy, disabling it from using attacking moves for the turn."
 # text.fill((255,0,0))
 
@@ -17,45 +16,40 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Test")
 
 surface = pygame.image.load("data/assets/cards/Common Lightning.png")
-surface_transformed = pygame.transform.scale(surface, (184, 165))
+# surface_transformed = pygame.transform.scale(surface, (184, 165))
 
+rect = pygame.Rect(40, 297, 300, 120)
 
-def drawText(surface, text, color, font, aa=False, bkg=None):
-    rect = pygame.Rect(42, 297, 280, 160)
-    y = rect.top
-    lineSpacing = -2
-
-    # get the height of the font
-    fontHeight = font.size("Tg")[1]
-
-    while text:
-        i = 1
-
-        # determine if the row of text will be outside our area
-        if y + fontHeight > rect.bottom:
-            break
-
-        # determine maximum width of line
-        while font.size(text[:i])[0] < rect.width and i < len(text):
-            i += 2
-
-        # if we've wrapped the text, then adjust the wrap to the last word
-        if i < len(text):
-            i = text.rfind(" ", 0, i) + 1
-
-        # render the line and blit it to the surface
-        if bkg:
-            image = font.render(text[:i], 1, color, bkg)
-            image.set_colorkey(bkg)
-        else:
-            image = font.render(text[:i], aa, color)
-
-        surface.blit(image, (rect.left, y))
-        y += fontHeight + lineSpacing
-
-        # remove the text we just blitted
-        text = text[i:]
-
+def blit_text(surface, text, rect:pygame.Rect, font_, default_size, color=pygame.Color('black')):
+    words = text.split(' ')  # 2D array where each row is a list of words.
+    max_width, max_height = rect.size
+    run = True
+    lines = [""]
+    size = default_size+1
+    while run:
+        size -= 1
+        font1 = pygame.font.SysFont(font_, size)
+        space = font1.size(' ')[0]  # The width of a space.
+        lines = [""]
+        index = 0
+        x, y = (0, 0)
+        for word in words:
+            word_surface = font1.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width + space >= max_width:
+                x = rect.topleft[0]  # Reset the x.
+                y += word_height  # Start on new row.
+                index += 1
+                lines.append("")
+            lines[index] += word + " "
+            x += word_width + space
+        if y <= rect.height:
+            run = False
+    x, y = rect.topleft
+    for line in lines:
+        text = font1.render(line, 0, color)
+        surface.blit(text, (rect.left, y))
+        y += text.get_size()[1]
     return text
 
 
@@ -66,10 +60,9 @@ while run:
             run = False
     screen.fill((255, 255, 255))
     screen.blit(surface, (0, 0))
-    screen.blit(cost, (65, 58))
+    screen.blit(cost, (64, 58))
     screen.blit(name, (115, 59))
-    # screen.blit(description, (42, 300))
-    drawText(screen, description, (0,0,0), font)
+    blit_text(surface, description, rect, "Minecraft", 26)
     pygame.display.update()
 pygame.quit()
 sys.exit()
